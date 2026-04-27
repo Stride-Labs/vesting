0c41af7d595ecf881b520455865a521bb45aadb5
sampocs <sam.pochyly@gmail.com>
2026-04-27 16:05:43 -0500

    bump to sdk54

--- x/vesting/keeper/keeper.go
+++ x/vesting/keeper/keeper.go
@@ -8,8 +8,8 @@ import (
 
 	"github.com/evmos/vesting/x/vesting/types"
 
-	"cosmossdk.io/log"
-	storetypes "cosmossdk.io/store/types"
+	"cosmossdk.io/log/v2"
+	storetypes "github.com/cosmos/cosmos-sdk/store/v2/types"
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
